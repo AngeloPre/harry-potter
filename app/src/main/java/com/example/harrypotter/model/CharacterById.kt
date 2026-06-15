56e1faa17337094401e36b4418f25c70b43fb18b
AngeloPre <angelo.prebianca@gmail.com>
2026-06-15 01:36:26 -0300

    FET: Adicionada telas de professores e personagens

--- app/src/main/java/com/example/harrypotter/model/CharacterById.kt
+++ app/src/main/java/com/example/harrypotter/model/CharacterById.kt
@@ -6,6 +6,10 @@ data class CharacterById(
     val name: String,
     val species: String,
     val house: String,
+    val alive: Boolean = true,
+    val wizard: Boolean = false,
+    val hogwartsStudent: Boolean = false,
+    val hogwartsStaff: Boolean = false,
     @SerializedName("image")
     val photo: String
 )
